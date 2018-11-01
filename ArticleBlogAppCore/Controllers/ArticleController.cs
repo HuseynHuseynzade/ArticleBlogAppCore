03f30e2dc08efcaa4f98d917d55c1f6e71519ada
Huseyn <Huseynzade@DESKTOP-F9FF7G6>
2018-11-01 16:20:48 +0400

    Create ViewComponents

---
+++ ArticleBlogAppCore/Controllers/ArticleController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArticleBlogAppCore.Data;
+using ArticleBlogAppCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArticleBlogAppCore.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private readonly ArticleBlogDbContext _articleBlogDbContext;
+        public ArticleController(ArticleBlogDbContext articleBlogDbContext)
+        {
+            _articleBlogDbContext = articleBlogDbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Detail(int id)
+        {
+            Article currentArticle = await _articleBlogDbContext.Articles.FindAsync(id);
+
+            if (currentArticle == null)
+            {
+                return NotFound();
+            }
+            return View(currentArticle);
+        }
+    }
+}
---
+++ ArticleBlogAppCore/ViewComponents/SliderViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using ArticleBlogAppCore.Data;
+using ArticleBlogAppCore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArticleBlogAppCore.ViewComponents
+{
+    public class SliderViewComponent:ViewComponent
+    {
+        private readonly ArticleBlogDbContext _articleBlogDbContext;
+
+        public SliderViewComponent(ArticleBlogDbContext articleBlogDbContext)
+        {
+            _articleBlogDbContext = articleBlogDbContext;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var articles = await _articleBlogDbContext.Articles.Take(4).Select(x => new ArticleIndexModel()
+            {
+                CreateDate = x.AddedDate,
+                Description = x.Description,
+                Id = x.Id,
+                Name = x.Name,
+                PhotoPath = x.PhotoPath,
+                Tags = x.Tags
+
+            }).ToListAsync();
+
+            return View(articles);
+        }
+    }
+}
